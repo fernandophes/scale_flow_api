af9cb4414189e60a227d4fbfba970006760da14d
Fernando Silva <fernandophes@gmail.com>
2025-10-21 16:46:22 -0300

    Configuração inicial

---
+++ src/test/java/br/edu/ufersa/cc/lpoo/scale_flow/ScaleFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.cc.lpoo.scale_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaleFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
