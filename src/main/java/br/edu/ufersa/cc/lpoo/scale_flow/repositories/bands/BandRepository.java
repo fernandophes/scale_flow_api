aa38bf942c4e838fb3dc814d5272aba9d027f9f2
Fernando Silva <fernandophes@gmail.com>
2025-10-22 18:14:45 -0300

    Bandas: repositórios

---
+++ src/main/java/br/edu/ufersa/cc/lpoo/scale_flow/repositories/bands/BandRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.cc.lpoo.scale_flow.repositories.bands;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufersa.cc.lpoo.scale_flow.entities.bands.Band;
+
+@Repository
+public interface BandRepository extends JpaRepository<Band, UUID> {
+
+}
--- src/main/java/br/edu/ufersa/cc/lpoo/scale_flow/repositories/users/UserRepository.java
+++ src/main/java/br/edu/ufersa/cc/lpoo/scale_flow/repositories/users/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.edu.ufersa.cc.lpoo.scale_flow.entities.users.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
